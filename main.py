22ce203379b5fe53a39a2a098826c38cf9c180d5
laptopboyboy <seonjunhwang383@gmail.com>
2024-09-28 12:03:26 +0900

    env change

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 import streamlit as st
 from openai import OpenAI
 
 #chatbot 기능 구현
 
+api_key = os.getenv('OPENAI_API_KEY')
 
 client = OpenAI(
-    api_key=OPENAI_API_KEY
+    api_key=api_key
 )
 
 instructions = """
